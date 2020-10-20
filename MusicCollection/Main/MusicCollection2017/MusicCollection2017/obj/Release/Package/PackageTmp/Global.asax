131ceacaa83795f9468fb412631cc088c122a008
Nigel <nigelsmyth@hotmail.com>
2020-10-20 22:22:58 +0100

    Published to Azure

---
+++ MusicCollection/Main/MusicCollection2017/MusicCollection2017/obj/Release/Package/PackageTmp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MusicCollection2017.MvcApplication" Language="C#" %>
---
+++ MusicCollection/Main/MusicCollection2017/MusicCollection2017/obj/Release/Package/PackageTmp/Scripts/PowerShell/ExtractArtists.ps1
@@ -0,0 +1,25 @@
+﻿#################################################################
+# Script to extract all artists from the dir specified in the first
+# line to 3 csv files.
+##################################################################
+
+$dir = "H:\My Music"
+cls
+
+if (test-path -path "$dir\Artist.csv")
+{
+    del "$dir\Artist.csv"
+}
+
+cd $dir
+$artists = Get-ChildItem -Path $dir
+
+foreach ($artist in $artists)
+{
+    if ($artist.Name -ne 'Artist.csv' -and $artist.Name -ne 'Album.csv'  -and $artist.Name -ne 'Songs.csv' )
+    {
+        Write-Host "Artist " = $artist.Name
+        $tempArtist = "('" + $artist.Name.Replace("'", "''") + "'), "
+        Write-Output $tempArtist | Out-File -Append  $dir'\Artist.csv'
+    }
+}
